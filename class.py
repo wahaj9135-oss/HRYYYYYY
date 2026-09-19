4784102a3dfb081ac1d42e2d6232dbf36c048a88
wahaj9135-oss <wahaj9135@gmail.com>
2026-09-19 14:31:29 +0000

    heyyyy

--- class.py
+++ class.py
@@ -1,17 +1,6 @@
-age = int(input("Please enter your age: "))
-ticket= int(input("Please enter the ticket price: "))
-if age < 5 :
-    print("You are eligible for a free ticket.")
-elif (age > 5 and age <= 12):
-    disc = ticket * (50/100)
-    print(f"You are eligible for a discount of ${disc:.2f}.")
-elif (age >12 and age <= 59):
-    print("You are not eligible for a free ticket.")
-elif (age >= 60):
-    disc = ticket * (30/100)
-    print(f"You are eligible for a discount of ${disc:.2f}.")   
-
-
-
-else:
-    print("You are not eligible for a free ticket.")
+class student:
+    def __init__(self, student_name):
+        self.student = student_name
+        print("hi")
+s1 = student("John")
+print(s1.student)
