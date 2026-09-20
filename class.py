d2ab1af805f9a067659bc7355d5702a7fe54ad6a
wahaj9135-oss <wahaj9135@gmail.com>
2026-09-20 07:42:43 +0000

    save hogia kia?

--- class.py
+++ class.py
@@ -1,6 +1,18 @@
-class student:
-    def __init__(self, student_name):
-        self.student = student_name
-        print("hi")
-s1 = student("John")
-print(s1.student)
+class teacher :
+    name = "anonymous"
+    def __init__(self, name):
+        self.name = name
+    @staticmethod
+    def fight(st1, st2):
+        print(f"{st1.name} is fighting with {st2.name}")
+        print("suspended both of them")
+    @staticmethod
+    def breaktime():
+        print("break time is over")
+st1 = teacher("john")
+st2 = teacher("jane")
+s4 = teacher("ali")
+s3 = teacher.fight(s4, st2)
+print(st1.name)
+print(st2.name)
+# print(s3)
